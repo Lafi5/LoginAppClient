a353107a3282d3e86a6f64e8991811c50cc1aacc
Lafi5 <vener55@freemail.hu>
2016-03-23 15:19:50 +0100

    User

--- LoginAppClient/User.cs
+++ LoginAppClient/User.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace LoginAppClient
 {
-    class User
+    [Serializable()]
+    [DataContract]
+    public class User
     {
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string Code { get; set; }
+
+        public User()
+        {
+            FirstName = "";
+            LastName = "";
+            Code = "";
+        }
+
+        public User(string firstname, string lastName, string code)
+        {
+            FirstName = firstname;
+            LastName = lastName;
+            Code = code;
+        }
+
+        public string GetName()
+        {
+            return String.Format("{0} {1}", FirstName, LastName);
+        }
     }
 }
